cf450b2c4aa0f4d23b2f0de2f80565676fd33306
dan-brown1 <db872@cam.ac.uk>
2022-02-01 23:49:35 +0000

    task 1B :)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,7 +6,23 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
 
-def stations_by_distance(stations,p):
-    
+from .utils import sorted_by_key
+from haversine import haversine
+
+
+def stations_by_distance(stations, p):
+    distance = []
+    stationname = []
+    towns = []
+    name_distance = []
+    for station in stations:
+        towns.append(station.town)
+        stationname.append(station.name)
+        distance.append(haversine(station.coord,p))
+    i=0
+    while i<len(stationname):
+        name_distance.append((stationname[i],towns[i],distance[i]))
+        i+=1
+    return sorted_by_key((name_distance),2)
+
