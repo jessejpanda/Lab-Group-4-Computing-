50e2b02e0b547bbc7bc0526817fd214694366201
dan-brown1 <db872@cam.ac.uk>
2022-02-07 12:11:21 +0000

    added docstrings for my tasks

--- test_geo.py
+++ test_geo.py
@@ -2,6 +2,7 @@ from floodsystem.geo import stations_by_distance,stations_within_radius,rivers_w
 from floodsystem.stationdata import build_station_list
 #test for funtion used in Task 1B
 def test_stations_by_distance():
+    """Tests to check that the outputs from funtion stations_by_distance are as expected"""
     stations = build_station_list()
     sorted_stations = stations_by_distance(stations, (52.2053, 0.1218)) 
     assert sorted_stations[0][2] < sorted_stations[1][2]
@@ -17,6 +18,7 @@ test_stations_by_distance()
 
 #Test for function used in 1C
 def test_stations_within_radius():
+    """Tests to check that the outputs from funtion stations_within_radius are as expected"""
     stations = build_station_list()
     sorted_stations = stations_within_radius(stations, (52.2053, 0.1218),10)
     for station in sorted_stations:
@@ -27,12 +29,12 @@ def test_stations_within_radius():
 
 #Test for task 1E
 def test_rivers_by_station_number():
+    """Tests to check that the outputs from funtion rivers_by_station_number are as expected"""
     stations = build_station_list()
-    test = rivers_by_station_number
-    for station in stations:
+    test = rivers_by_station_number(stations, 9)
+    for station in test:
         assert type(station) is tuple 
-        assert type(station[0]) is str
-        assert type(station[1]) is float
+        assert type(station[1]) is int
     i=0
     for i in range(0,len(test)-1):
         assert test[i][1] >= test[i+1][1]
