925727e0c15f2370926778d3b84117acb9931f4a
John Doe <johndoe@example.com>
2022-02-07 22:11:39 +0000

    Task 1F

--- Task1F.py
+++ Task1F.py
@@ -1,5 +1,11 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key
 from floodsystem.station import inconsistent_typical_range_stations
 stations = build_station_list()
-inconsistent = sorted(excluded_stations)
+excluded_stations = inconsistent_typical_range_stations(stations)
+excluded_stations_and_names = []
+for station in excluded_stations:
+    excluded_stations_and_names.append(station.name)
+inconsistent = sorted(excluded_stations_and_names)
+
 print(inconsistent)
