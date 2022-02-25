32e5b49ae731a86fed5fc3182278798a3a5158c1
dan-brown1 <db872@cam.ac.uk>
2022-02-25 00:20:27 +0000

    task 2f

---
+++ floodsystem/Analysis.py
@@ -0,0 +1,15 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Task2F
+def polyfit(dates, levels, p):
+    x = matplotlib.dates.date2num(dates)
+    x1= x[0]
+    x = x-x1
+    p_coeff = np.polyfit(x, levels, p)
+    poly = np.poly1d(p_coeff)
+    return poly,x1
+
