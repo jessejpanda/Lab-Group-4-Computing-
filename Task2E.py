e712a77f230b839fbb01ec36f1072d8bffb41154
John Doe <johndoe@example.com>
2022-02-26 21:58:31 +0000

    2e

--- Task2E.py
+++ Task2E.py
@@ -12,13 +12,27 @@ def run():
     update_water_levels(stations)
     stations_at_risk = stations_highest_rel_level(stations, 5)
 
+def run():
+
+    # Build stations
+    stations = build_station_list()
+
+    # Update station data
+    update_water_levels(stations)
+
+    # Get top 5 stations
+    stations_at_risk = stations_highest_rel_level(stations, 5)
+ 
+
+    # Gather level data for rivers
     dt = 10
 
     for station in stations_at_risk:
-        dates,levels = fetch_measure_levels(
-            station.measure_id, dt=datetime.timedelta(days=dt))
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+ 
         plot_water_levels(station, dates, levels)
-    return
+        
+    
     
 
 if __name__ == "__main__":
