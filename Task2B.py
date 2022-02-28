73887c56757308393c9fee0743127f66df1f3db6
dan-brown1 <db872@cam.ac.uk>
2022-02-28 20:54:42 +0000

    yml

--- Task2B.py
+++ Task2B.py
@@ -5,7 +5,8 @@ stations = build_station_list()
 update_water_levels(stations)
 for station_tuple in stations_level_over_threshold(stations, 0.8):
     print(station_tuple[0].name, station_tuple[1])
-    
+ #The station tuple is the station data and relative water level   
+
 
 
     
