8eb072583195ebe40196c0ac2d1ec1f85a458241
Jesse Panda <jp938@cam.ac.uk>
2022-02-28 19:25:25 +0000

    2e final

--- Task2E.py
+++ Task2E.py
@@ -1,27 +1,30 @@
+from floodsystem.stationdata import *
+from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
+import matplotlib.pyplot as plt
 
+def run():
 
+    # Build stations
+    stations = build_station_list()
 
-
-
-
-
-
-
-
-
-
+    # Update station data
+    update_water_levels(stations)
     
+    # Get top 5 stations
+    stations_at_risk = stations_highest_rel_level(stations, 5)
 
+    # Gather level data for rivers
+    dt = 10
+    for station in stations_at_risk:
+        dates, levels = fetch_measure_levels(station[0].measure_id,\
+        dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
 
+if __name__=="__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
 
-
-
-
-
-  
-
-        
-
-
-
-# %%
+    # Run Task2E
+    run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from floodsystem.Analysis import polyfit
 
+#Task 2F
 def plot_water_level_with_fit(station, dates, levels, p):
     plt.plot(dates,levels,'.',label =("Past measuremnts of relative water level"))
     poly,x1 = polyfit(dates,levels,p)
@@ -24,6 +25,7 @@ def plot_water_level_with_fit(station, dates, levels, p):
 #upper typical range when relative water level = 1
 #lower typical range when relaytive water level = 0
 
+#Task 2E
 def plot_water_levels(station, dates, levels):
     """Takes an input of dates and water levels and plots both on a graph.
     Also shows the typical high and low for the station on the same graph.
@@ -33,16 +35,14 @@ def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels, label="$water levels$")
 
     # add lines of typical high and low
-    plt.plot([dates[-1], dates[0]], [station.typical_range[0],
-                                     station.typical_range[0]], color='g', label="$typical low$")
-    plt.plot([dates[-1], dates[0]], [station.typical_range[1],
-                                     station.typical_range[1]], color='r', label="$typical high$")
-
+    plt.axhline(station[0].typical_range[1], color='brown', label="$typical low$")
+    plt.axhline(station[0].typical_range[0], color='blue', label="$typical high$")
+    
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45)
-    plt.title("Station: " + station.name)
+    plt.title("Station: " + station[0].name)
 
     # Display stuff
     plt.tight_layout()  # This makes sure plot does not cut off date labels
