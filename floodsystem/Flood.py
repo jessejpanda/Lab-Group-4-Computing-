1321086539eb5b7513174fd0db9c066ca4ab9b1c
dan-brown1 <db872@cam.ac.uk>
2022-02-28 19:23:48 +0000

    tests except for 2F

--- floodsystem/Flood.py
+++ floodsystem/Flood.py
@@ -2,6 +2,8 @@ from .utils import sorted_by_key
 from .station import MonitoringStation
 #Task2B
 def stations_level_over_threshold(stations, tol):
+    """Function which takes the list of all stations and a tolerance level. Returns a tuple of station name and relative water level for stations
+    who's relative water level is over the tol """
     stations_over_tol = []
     for station in stations:
         if station.relative_water_level() == None:
@@ -15,6 +17,7 @@ def stations_level_over_threshold(stations, tol):
 
 #Task2C
 def stations_highest_rel_level(stations, N):
+    """Takes a list of all monitoring stations and a number N as an input. Returns the top N stations with the highest relative water level as a tuple of station name and relative water level. """
     level_relative_to_range = stations_level_over_threshold(stations,0)
     return level_relative_to_range[:N]
     
